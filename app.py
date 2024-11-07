9ddeca695a799d130c1838d44c3f0d47785a69ab
keneokeke <kenechu.okeke@gmail.com>
2024-11-07 07:35:17 -0500

    created new html page

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def newflask():
     return "<h1>Welcome</h2>"
+
+@app.route("/test")
+def newdev():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 8000)
